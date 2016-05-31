67ad4c247b81f9ed0e8952a288f5899867344c38
channabelle <439593353@qq.com>
2016-05-31 15:48:01 +0800

    前端框架模板修改

---
+++ dev/fis-conf.js
@@ -0,0 +1,37 @@
+// 加 md5
+fis.match('*.{js,css}', {
+    useHash: true
+});
+
+// 启用 fis-spriter-csssprites 插件
+fis.match('::package', {
+    spriter: fis.plugin('csssprites')
+})
+
+// 对 CSS 进行图片合并
+fis.match('*.css', {
+    // 给匹配到的文件分配属性 `useSprite`
+    useSprite: true
+});
+
+fis.match('*.js', {
+    // fis-optimizer-uglify-js 插件进行压缩，已内置
+    optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+    // fis-optimizer-clean-css 插件进行压缩，已内置
+    optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+    // fis-optimizer-png-compressor 插件进行压缩，已内置
+    optimizer: fis.plugin('png-compressor')
+});
+
+/*
+ * 发布地址
+ */
+fis.match('*.{html,png,js,css,jpg,gif,otf}', {
+    release: 'DreamWorks/rel/$0'
+});
